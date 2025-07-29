efdf43b232cd3b9870d85bb65d9ab77bc1f6097c
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-07-29 19:53:05 +0700

    Update .gitignore.lua

--- .gitignore.lua
+++ .gitignore.lua
@@ -30,8 +30,8 @@ local allowedGames = {
     ["116495829188952"] = {name = "Dead Rails", url = "https://raw.githubusercontent.com/dyumra/DYHUB-Universal-Game/refs/heads/main/Drlobby.lua"},
     ["70876832253163"] = {name = "Dead Rails [In-game]", url = "https://raw.githubusercontent.com/dyumra/DYHUB-Universal-Game/refs/heads/main/DeadRails.lua"},
     ["136806264049477"] = {name = "Keys and Knives", url = "https://raw.githubusercontent.com/gumanba/Scripts/main/KeysandKnives"},
-    ["79546208627805"] = {name = "99 Nights in the Forest", url = "https://raw.githubusercontent.com/dyumra/dyumrascript-/refs/heads/main/99dayv2new.lua"},
-    ["126509999114328"] = {name = "99 Nights in the Forest [In-game]", url = "https://raw.githubusercontent.com/dyumra/dyumrascript-/refs/heads/main/99dayv2new.lua"},
+    ["79546208627805"] = {name = "99 Nights in the Forest", url = "https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/dddd.lua"},
+    ["126509999114328"] = {name = "99 Nights in the Forest [In-game]", url = "https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/dddd.lua"},
     ["110931811137535"] = {name = "Feed a Brainrot", url = "https://raw.githubusercontent.com/dyumra/DYHUB-Universal-Game/refs/heads/main/fyb.lua"},
     ["137925884276740"] = {name = "Build A Plane", url = "https://raw.githubusercontent.com/dyumra/DYHUB-Universal-Game/refs/heads/main/BAP.lua"},
 }
