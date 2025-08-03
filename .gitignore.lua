ce6708e2008169d97d6090f15e4a7cae251b5c55
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-08-04 02:46:12 +0700

    Update .gitignore.lua

--- .gitignore.lua
+++ .gitignore.lua
@@ -267,7 +267,7 @@ local premiumUsers = {
 -- ====================== Give Away ============================
 
     ["Masayoshi88"] = {Tag = "musashi_0940", Time = "Time: Lifetime"}, -- First
-    ["0"] = {Tag = "0", Time = "Days: 1"},
+    ["Kyn8s"] = {Tag = "kyn8s", Time = "Days: 1"},
     ["0"] = {Tag = "0", Time = "Days: 1"},
     ["0"] = {Tag = "0", Time = "Days: 1"},
 }
