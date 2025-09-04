c3b1dbfd947386d18251ed29e0a21a88e4b865f4
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-09-04 23:01:21 +0700

    Update dyhub99night.lua

--- dyhub99night.lua
+++ dyhub99night.lua
@@ -16,3 +16,16 @@ getgenv().hop = "old"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/F99D.lua"))()
 ]]
 queue_on_teleport(queueScript)
+
+repeat wait(0.1) until game.Players
+local TimeWaitLoadGame = 0
+local player = game.Players.LocalPlayer
+repeat
+	wait(0.1)
+	TimeWaitLoadGame = TimeWaitLoadGame + 0.1
+	if TimeWaitLoadGame > 3 then
+		player:Kick()
+		wait(0.5)
+		game:GetService("TeleportService"):Teleport(126509999114328)
+	end
+until game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
