59452b66dc7a32d4c797c1ec8bf8be18997bc660
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-09-04 23:16:02 +0700

    Update dyhub99night.lua

--- dyhub99night.lua
+++ dyhub99night.lua
@@ -10,10 +10,7 @@ getgenv().hop = "old"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/F99D.lua"))()
 
 local queueScript = [[
-getgenv().WebhookURL = "https://discord.com/api/webhooks/1413180796143013920/9ZOx_ukN2ucZGdQUmkITMUpQPzQgZWruKtDw8ebmYiSWkWjfN7Lp8fQew9_9KBaLYajV"
-getgenv().AutoFarm = true
-getgenv().hop = "old"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/F99D.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/dyhub99night.lua"))()
 ]]
 queue_on_teleport(queueScript)
 
