e62415ce6944435a40f22b1fa0ccac95bafbbc5f
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-09-04 22:24:08 +0700

    Update dyhub99night.lua

--- dyhub99night.lua
+++ dyhub99night.lua
@@ -1,4 +1,18 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+if not game:IsLoaded() then repeat task.wait() until game:IsLoaded() end
+
+if getgenv().RunScript == true then return end
+getgenv().RunScript = true
+
 getgenv().WebhookURL = "https://discord.com/api/webhooks/1413180796143013920/9ZOx_ukN2ucZGdQUmkITMUpQPzQgZWruKtDw8ebmYiSWkWjfN7Lp8fQew9_9KBaLYajV"
 getgenv().AutoFarm = true
 getgenv().hop = "old"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/F99D.lua"))()
+
+local queueScript = [[
+getgenv().WebhookURL = "https://discord.com/api/webhooks/1413180796143013920/9ZOx_ukN2ucZGdQUmkITMUpQPzQgZWruKtDw8ebmYiSWkWjfN7Lp8fQew9_9KBaLYajV"
+getgenv().AutoFarm = true
+getgenv().hop = "old"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/F99D.lua"))()
+]]
+queue_on_teleport(queueScript)
