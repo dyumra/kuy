90731aa1242007d47a2362ec54c2aac5667856c3
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-09-04 22:16:56 +0700

    Create dyhub99night.lua

---
+++ dyhub99night.lua
@@ -0,0 +1,4 @@
+getgenv().WebhookURL = "https://discord.com/api/webhooks/1413180796143013920/9ZOx_ukN2ucZGdQUmkITMUpQPzQgZWruKtDw8ebmYiSWkWjfN7Lp8fQew9_9KBaLYajV"
+getgenv().AutoFarm = true
+getgenv().hop = "old"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/F99D.lua"))()
